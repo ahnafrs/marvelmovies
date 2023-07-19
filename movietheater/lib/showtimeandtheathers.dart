a6a8a8174cd75b7916664abb51a69347c762686a
Ahnaf <ahnafrahman71@gmail.com>
2023-07-19 12:57:13 +0600

    Design Done

--- movietheater/lib/showtimeandtheathers.dart
+++ movietheater/lib/showtimeandtheathers.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:movietheater/movielisting.dart';
+import 'package:movietheater/paymentpage.dart';
 
 class ShowtimeAndTheaters extends StatefulWidget {
   const ShowtimeAndTheaters({Key? key, required this.movie}) : super(key: key);
@@ -12,8 +13,10 @@ class ShowtimeAndTheaters extends StatefulWidget {
 
 class _ShowtimeAndTheatersState extends State<ShowtimeAndTheaters> {
   late String imageUrl;
+  List<int> selectedSeats = [];
   int selectedDateIndex = -1;
-  int selectedSeatIndex = -1;
+  int selectedShowtimeIndex = -1;
+  final double ticketPrice = 400.0;
 
   @override
   void initState() {
@@ -23,6 +26,8 @@ class _ShowtimeAndTheatersState extends State<ShowtimeAndTheaters> {
 
   @override
   Widget build(BuildContext context) {
+    double totalPrice = ticketPrice * selectedSeats.length;
+
     return Scaffold(
       backgroundColor: Colors.black,
       body: Column(
@@ -33,7 +38,7 @@ class _ShowtimeAndTheatersState extends State<ShowtimeAndTheaters> {
             child: Center(
               child: Image.network(
                 imageUrl,
-                fit: BoxFit.contain,
+                fit: BoxFit.fitWidth,
               ),
             ),
           ),
@@ -49,16 +54,20 @@ class _ShowtimeAndTheatersState extends State<ShowtimeAndTheaters> {
                 ),
                 itemCount: 30,
                 itemBuilder: (context, index) {
-                  final isSelected = index == selectedSeatIndex;
+                  final isSelected = selectedSeats.contains(index);
                   return GestureDetector(
                     onTap: () {
                       setState(() {
-                        selectedSeatIndex = index;
+                        if (isSelected) {
+                          selectedSeats.remove(index);
+                        } else {
+                          selectedSeats.add(index);
+                        }
                       });
                     },
                     child: Container(
                       decoration: BoxDecoration(
-                        color: isSelected ? Colors.grey[400] : Colors.white,
+                        color: isSelected ? Colors.red : Colors.white,
                         border: Border.all(
                           color: Colors.grey[400]!,
                           width: 1.0,
@@ -132,7 +141,6 @@ class _ShowtimeAndTheatersState extends State<ShowtimeAndTheaters> {
                 scrollDirection: Axis.horizontal,
                 itemCount: 10,
                 itemBuilder: (context, index) {
-                  final isSelected = index == selectedDateIndex;
                   return GestureDetector(
                     onTap: () {
                       setState(() {
@@ -146,7 +154,9 @@ class _ShowtimeAndTheatersState extends State<ShowtimeAndTheaters> {
                         horizontal: 16.0,
                       ),
                       decoration: BoxDecoration(
-                        color: isSelected ? Colors.grey[400] : Colors.white,
+                        color: selectedDateIndex == index
+                            ? Colors.grey[400]
+                            : Colors.white,
                         borderRadius: BorderRadius.circular(30.0),
                       ),
                       child: Center(
@@ -155,7 +165,9 @@ class _ShowtimeAndTheatersState extends State<ShowtimeAndTheaters> {
                           style: TextStyle(
                             fontSize: 16.0,
                             fontWeight: FontWeight.bold,
-                            color: isSelected ? Colors.white : Colors.black,
+                            color: selectedDateIndex == index
+                                ? Colors.white
+                                : Colors.black,
                           ),
                         ),
                       ),
@@ -167,15 +179,89 @@ class _ShowtimeAndTheatersState extends State<ShowtimeAndTheaters> {
           ),
           Expanded(
             flex: 1,
-            child: Placeholder(),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedShowtimeIndex = index;
+                      });
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.all(8.0),
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 8.0,
+                        horizontal: 16.0,
+                      ),
+                      decoration: BoxDecoration(
+                        color: selectedShowtimeIndex == index
+                            ? Colors.grey[400]
+                            : Colors.white,
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      child: Center(
+                        child: Text(
+                          '5:30 PM',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                            color: selectedShowtimeIndex == index
+                                ? Colors.white
+                                : Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
           ),
-          Expanded(
-            flex: 1,
-            child: Placeholder(),
-          ),
-          Expanded(
-            flex: 1,
-            child: Placeholder(),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Total: BDT $totalPrice',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => PaymentPage()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.red,
+                    padding: EdgeInsets.symmetric(
+                      vertical: 16.0,
+                      horizontal: 24.0,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                  ),
+                  child: Text(
+                    'Buy Ticket',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
